c704ad0ed5d8f580ab2fb36c93702d8442662620
nbgao <nbgao@126.com>
2019-07-30 20:27:43 +0800

    Add MODEL_USE & Fix Cfgs

--- openvqa/models/mfh/model_cfgs.py
+++ openvqa/models/mfh/model_cfgs.py
@@ -13,9 +13,9 @@ class Cfgs(BaseCfgs):
         self.HIGH_ORDER = False
         self.HIDDEN_SIZE = 512
         self.MFB_FACTOR_NUM = 5
-        self.MFB_OUT_SIZE = 1024
+        self.MFB_OUT_SIZE = 1000
         self.LSTM_OUT_SIZE = 1024
-        self.LSTM_DROPOUT_RATIO = 0.3
+        self.LSTM_DROPOUT_RATIO = 0.1
         self.MFB_DROPOUT_RATIO = 0.1
         self.NUM_IMG_GLIMPSES = 2
         self.NUM_QUES_GLIMPSES = 2
