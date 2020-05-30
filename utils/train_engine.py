4d511ce28dfa83cb72e703303f2ea87686d9d8a0
ParadoxZW <964730078@qq.com>
2020-05-30 17:28:33 +0800

    fix grad_accu bug

--- utils/train_engine.py
+++ utils/train_engine.py
@@ -186,7 +186,9 @@ def train_engine(__C, dataset, dataset_eval=None):
                         loss_item[item_ix] = eval('F.' + loss_nonlinear + '(loss_item[item_ix], dim=1)')
 
                 loss = loss_fn(loss_item[0], loss_item[1])
-                loss /= __C.GRAD_ACCU_STEPS
+                if __C.LOSS_REDUCTION == 'mean':
+                    # only mean-reduction needs be divided by grad_accu_steps
+                    loss /= __C.GRAD_ACCU_STEPS
                 loss.backward()
 
                 loss_tmp += loss.cpu().data.numpy() * __C.GRAD_ACCU_STEPS
